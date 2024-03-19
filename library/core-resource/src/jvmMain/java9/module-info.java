654251e915df742b40f71386f26937b8d59478f7
Matthew Nelson <developer@matthewnelson.io>
2024-03-19 10:54:14 -0400

    Add JPMS support via Multi-Release Jar

---
+++ library/core-resource/src/jvmMain/java9/module-info.java
@@ -0,0 +1,6 @@
+module io.matthewnelson.kmp.tor.core.resource {
+    requires transitive io.matthewnelson.kmp.tor.core.api;
+    requires io.matthewnelson.immutable.collections;
+
+    exports io.matthewnelson.kmp.tor.core.resource;
+}
