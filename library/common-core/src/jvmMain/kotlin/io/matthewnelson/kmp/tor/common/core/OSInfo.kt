fe0e4758973573395585bf9910bfa1201b057002
Matthew Nelson <developer@matthewnelson.io>
2025-06-10 12:43:30 -0400

    Deprecate OSInfo.isAndroidRuntime()

--- library/common-core/src/jvmMain/kotlin/io/matthewnelson/kmp/tor/common/core/OSInfo.kt
+++ library/common-core/src/jvmMain/kotlin/io/matthewnelson/kmp/tor/common/core/OSInfo.kt
@@ -20,7 +20,6 @@ package io.matthewnelson.kmp.tor.common.core
 
 import io.matthewnelson.kmp.file.ANDROID
 import io.matthewnelson.kmp.file.File
-import io.matthewnelson.kmp.file.IOException
 import io.matthewnelson.kmp.file.toFile
 import io.matthewnelson.kmp.tor.common.api.InternalKmpTorApi
 import io.matthewnelson.kmp.tor.common.core.internal.ARCH_MAP
@@ -79,7 +78,7 @@ public actual class OSInfo private constructor(
             hostNameLC.contains("darwin") -> OSHost.MacOS
             hostNameLC.contains("freebsd") -> OSHost.FreeBSD
             hostNameLC.contains("linux") -> when {
-                isAndroidRuntime() -> OSHost.Linux.Android
+                ANDROID.SDK_INT != null -> OSHost.Linux.Android
                 isAndroidTermux() -> OSHost.Linux.Android
                 isLinuxMusl() -> OSHost.Linux.Musl
                 else -> OSHost.Linux.Libc
@@ -101,8 +100,6 @@ public actual class OSInfo private constructor(
 
         OSArch.Unsupported(archName.replace("\\W", "").lowercase(Locale.US))
     }
-
-    public fun isAndroidRuntime(): Boolean = ANDROID.SDK_INT != null
 
     private fun isAndroidTermux(): Boolean = try {
         process.runAndWait(listOf("uname", "-o"))
@@ -247,4 +244,13 @@ public actual class OSInfo private constructor(
         // Unsupported
         return null
     }
+
+    @Deprecated(
+        message = "Ambiguity in function name.",
+        replaceWith = ReplaceWith(
+            "ANDROID.SDK_INT != null",
+            "io.matthewnelson.kmp.file.ANDROID",
+        ),
+    )
+    public fun isAndroidRuntime(): Boolean = ANDROID.SDK_INT != null
 }
